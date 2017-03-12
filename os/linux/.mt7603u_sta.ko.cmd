a99bc13905d59df14cb8708a79387907e26357ee
phuslu <phuslu@noreply.github.com>
2017-03-12 11:37:24 +0800

    remove tmp files

--- os/linux/.mt7603u_sta.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/kev/WorkSpace/Project/MT7603U/os/linux/mt7603u_sta.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/kev/WorkSpace/Project/MT7603U/os/linux/mt7603u_sta.ko /home/kev/WorkSpace/Project/MT7603U/os/linux/mt7603u_sta.o /home/kev/WorkSpace/Project/MT7603U/os/linux/mt7603u_sta.mod.o ;  true
